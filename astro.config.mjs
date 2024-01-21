bc33f5c2ae5f718e416fa9235a0a899cb93c9132
Moriel Schottlender <moriel@gmail.com>
2024-01-21 02:31:11 +0200

    Add netlify; remove unused vue module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  output: "server",
+  adapter: netlify()
 });
