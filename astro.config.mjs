1e73f1b9225ff1e633d7013423a81bf9ea29e875
Moriel Schottlender <moriel@gmail.com>
2024-01-22 05:00:14 +0200

    Add switcher, redesign content and add intro for the switcher pieces

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import netlify from "@astrojs/netlify"
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  redirects: {
+    '/about': '/'
+  }
 });
