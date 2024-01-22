7c217408028fa296d5496b9c9efeb5683f71e0b5
Moriel Schottlender <moriel@gmail.com>
2024-01-22 18:12:17 +0200

    Following up on astrojs docs - revert to 'server' with optout prerender

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid", //"server",
+  output: "server",// "hybrid",
   adapter: netlify(),
   redirects: {
     '/about': '/'
