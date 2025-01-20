6c6df90831b32f09bee857be9ffc1d3f4cfa13cc
mooeypoo <moriel@gmail.com>
2025-01-20 11:52:13 -0500

    Add ssr instruction to nuxt config for netlify forms

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: true,
   app: {
     head: {
       title: 'Moriel Schottlender : Technically Funny',
